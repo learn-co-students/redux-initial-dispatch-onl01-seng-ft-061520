d5f5d384a2b35a2620d46efc04405bb44e4e17ab
Soundwood <j.benhogan@gmail.com>
2020-10-28 09:00:37 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,17 +1,19 @@
-let state = {count: 0};
+let state
 
-function changeState(state, action){
-    switch (action.type) {
-      case 'INCREASE_COUNT':
-        return {count: state.count + 1}
-      default:
-        return state;
-    }
+function changeState(state = { count: 0 }, action) {
+  switch (action.type) {
+
+    case 'INCREASE_COUNT':
+      return { count: state.count + 1 }
+
+    default:
+      return state;
   }
+}
 
 function dispatch(action){
-    state = changeState(state, action)
-    render()
+state = changeState(state, action)
+render()
 }
 
 function render(){
